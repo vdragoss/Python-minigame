937c81c2eb3d6cbb9fc47150deb26c2982387ee0
vdragoss <v.dragoss@gmail.com>
2019-10-10 17:17:27 +0300

    Got walking through rooms working

--- rooms.py
+++ rooms.py
@@ -7,22 +7,61 @@ with open("rooms.json") as f:
 
 class Room(object):
 
-    def __init__ (self, name, description, contents, exits):
-        self.name = name
-        self.description = description
-        self.contents = contents
-        self.exits = exits
-        self.Action = Action()
+    def __init__ (self, name):
+        self.name = d[f'{name}']['Name']
+        self.description = d[f'{name}']['Description']
+        self.description2 = d[f'{name}']['Description2']
+        self.contents = d[f'{name}']['Contents']
+        self.exits = d[f'{name}']['Exits']
+        self.visited = False
 
     def enter(self):
-        print("enter", Description)
 
-    def attack(self):
-        self.Action.attack()
+        if self.visited == False:
+            print("enter", self.description)
+            self.visited = True
+        else:
+            print("enter", self.description2)
 
-    def pickUp(self):
-        self.Action.pickUp()
+class LivingRoom(Room):
+    pass
+
+class Kitchen(Room):
+    pass
+
+class Study(Room):
+    pass
+
+class Hallway(Room):
+    pass
+
+class Yard(Room):
+    pass
 
 
-LivingRoom = Room(d['1']['Name'],d['1']['Description'],d['1']['Contents'],d['1']['Exits'])
-print(LivingRoom.name, LivingRoom.exits)
+
+
+rooms = {
+        'Living Room' : LivingRoom("Living Room"),
+        'Kitchen' : Kitchen("Kitchen"),
+        'Hallway' : Hallway("Hallway"),
+        'Study' : Study("Study"),
+        'Yard' : Yard("Yard")
+        }
+# LivingRoom.enter()
+# LivingRoom.enter()
+# Kitchen.enter()
+# Kitchen.enter()
+# LivingRoom.enter()
+
+current_room = rooms['Living Room']
+next_room = ""
+print(current_room.name)
+while True:
+    if current_room != next_room:
+        current_room.enter()
+    next_room = input("Where do you want to go next?")
+    if next_room in current_room.exits:
+        current_room = rooms[next_room]
+    else:
+        print("Try again")
